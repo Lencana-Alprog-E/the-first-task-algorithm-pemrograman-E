a6f703c19e064cc2ab333db370bc88a1313ae120
Lencana <lensa918@gmail.com>
2024-11-08 09:57:17 +0800

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,24 @@
+#variabel assigment value
+x = 3
+y = 2
+print("nilai dari x adalah : ", x)
+
+#aturan penamaan variabel
+luas = 85 # bisa berupa kata
+siapa_aja = 6000 # jika dua kata, digabung dg "_"
+Ayax = 800 # boleh digabungcapital non-capital
+ayax = 900
+lencan3 = 999 # jika menggabungkan huruf dan angka,angkanya diakhir
+print("nilai dariayax adalah : ", lencan3)
+
+a = 8 # integer bilangan bulat
+print("tipe data dari a :", ttype (a))
+
+b = 9,9 # float bilangan pecahan 
+print("tipe data dari b : ", type (b))
+
+c = "keren" # string, gabungan pecahan
+print("tipe data dari c : ", type (c))
+
+d = False # boolean, true false
+preint("tipe data dri d : ", type (d))
