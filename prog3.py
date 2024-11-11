e217fdd63429c61236860afb64d9ce498293cdb0
Lencana <lensa918@gmail.com>
2024-11-11 09:52:26 +0800

    Update and rename prog.py to prog3.py

--- prog3.py
+++ prog3.py
@@ -6,7 +6,7 @@ print("nilai dari x adalah : ", x)
 #aturan penamaan variabel
 luas = 85 # bisa berupa kata
 siapa_aja = 6000 # jika dua kata, digabung dg "_"
-Ayax = 800 # boleh digabungcapital non-capital
+Ayax = 800 # boleh digabung capital non-capital
 ayax = 900
 lencan3 = 999 # jika menggabungkan huruf dan angka,angkanya diakhir
 print("nilai dariayax adalah : ", lencan3)
