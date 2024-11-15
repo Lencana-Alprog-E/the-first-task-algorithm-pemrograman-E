74d9b0e620e80db61c0cd161a159f742e7452093
Lencana <lensa918@gmail.com>
2024-11-15 21:10:13 +0800

    Add files via upload

---
+++ prog7.py
@@ -0,0 +1,10 @@
+# modifikasi program 5 agar mengandung user
+
+a = int(input ("masukan alas"))
+print ("nilai dari a adalah : ", a, type (a))
+
+b = int(input ("masukan tinggi"))
+print ("nilai dari b adalah : ", b, type (b))
+
+hasil = 1/2*a*b
+print ("hasil dari 1/2*a*b adalah : ", hasil)
