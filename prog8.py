d628291d421b0707d429c6b054544b711fa4636a
Lencana <lensa918@gmail.com>
2024-11-29 09:37:15 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,47 @@
+# aritmatika
+# penjumlahan 
+x = 24
+y = 89
+hasil = x + y
+print ("nilai dari x + y = ", hasil) 
+
+# pengurangan
+hasil = x - y
+print ("nilai dari x - y = ", hasil) 
+
+# pembagian
+hasil = x / y
+print ("nilai dari x / y = ", hasil) 
+
+# perkalian
+hasil = x * y
+print ("nilai dari x * y = ", hasil) 
+
+# eksponen ( perpangkatan)
+hasil = x ** y
+print ("nilai dari x ** y = ", hasil) 
+
+# modulus ( sisa bagi)
+hasil = x // y
+print ("nilai dari x // y = ", hasil) 
+
+# Floor division
+hasil = x // y
+print ("nilai dari x FD y = ", hasil) 
+
+# operasi Prioritas
+# 1. kerjakan dari kiri ke kanan 
+# 2. perpangkatan perkalian/pembagian pengurangan/penambahan
+
+
+
+
+
+
+
+
+
+
+
+
+
